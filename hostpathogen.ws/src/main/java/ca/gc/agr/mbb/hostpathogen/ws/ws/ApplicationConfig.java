c9fc03baf5607d0204d16d353a8ecc9c5d29f981
ktcloud <van.kristy@gmail.com>
2015-05-22 16:00:11 -0400

    remove hostpathogen.ws folder

--- hostpathogen.ws/src/main/java/ca/gc/agr/mbb/hostpathogen/ws/ws/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package ca.gc.agr.mbb.hostpathogen.ws.ws;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
- 
-@ApplicationPath(value = "pathogen")
-public class ApplicationConfig extends Application {
-}
